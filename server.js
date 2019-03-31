67e38763c3b7b2007603fd00ce2575a653278604
ilyandho <ii.ndhote@gmail.com>
2019-03-31 18:03:19 +0300

    Created the GET,POST,DELETE routers to our API

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+//set the request to routes/api point to items
+const items = require("./routes/api/items");
+
 const app = express();
 
 //BodyParser middleware
@@ -17,6 +20,10 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDB connected ..."))
   .catch(err => console.log(err));
+
+//Use Routes
+//Anythin that points to /api/items should refer to the items variable
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 
